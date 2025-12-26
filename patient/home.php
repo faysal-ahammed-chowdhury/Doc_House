9a7962daaf395394cd703afe284cb53584be77a1
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2025-12-26 15:30:01 +0600

    Complete Patient Home Page Markup

---
+++ patient/home.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <nav>
+      <div class="container">
+        <div class="content">
+          <a href=""
+            ><img class="logo" src="../images/logo.png" alt="logo"
+          /></a>
+          <div class="links">
+            <a href="" class="link active">Home</a>
+            <a href="" class="link">Find a Doctor</a>
+            <a href="" class="link">My Appointments</a>
+          </div>
+          <div class="user-profile-and-logout">
+            <div class="user">
+              <p class="hello">Hello</p>
+              <p class="name"><a href="">Faysal Chowdhury</a></p>
+            </div>
+            <div class="logout">
+              <a href="">Logout</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="hero-section">
+      <h1>Your Health Is Our Top Priority</h1>
+      <p class="gray-para">Book appointments with top specialists.</p>
+      <a href="" class="primary-btn">Find a Doctor</a>
+    </section>
+
+    <section class="specialization-section">
+      <div class="container">
+        <div class="header">
+          <h2>Browse by Specialization</h2>
+          <p class="gray-para">Select a category to view available doctors.</p>
+        </div>
+
+        <div class="all-specilization">
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+          <div class="card">
+            <h3><a href="">Dentist</a></h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
