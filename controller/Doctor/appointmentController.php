b03bb4b34a5c5b2cc5360712d7ea2788965b3074
Siyam Talukder <mdsiyamtalukder1@gmail.com>
2026-01-12 08:59:52 +0600

    updating middleware

--- controller/Doctor/appointmentController.php
+++ controller/Doctor/appointmentController.php
@@ -1,8 +1,13 @@
 <?php
-session_start();
 require_once __DIR__ . '/../../model/Doctor/appointment_model.php';
 
-$doctor_id = 11; 
+if (isset($_SESSION['user']['did'])) {
+    $doctor_id = $_SESSION['user']['did'];
+} elseif (isset($_SESSION['did'])) {
+    $doctor_id = $_SESSION['did'];
+} else {
+    $doctor_id = 11;
+}
 
 $filterDate = isset($_GET['date']) ? $_GET['date'] : '';
 $filterStatus = isset($_GET['status']) ? $_GET['status'] : '';
