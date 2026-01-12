0f6ee570b683d861d5dde2cd288c13cc29c67ce0
Siyam Talukder <mdsiyamtalukder1@gmail.com>
2026-01-12 08:46:52 +0600

    appointment model implementation

---
+++ model/Doctor/appointment_model.php
@@ -0,0 +1,61 @@
+<?php
+require_once 'db.php';
+
+function getAppointmentsByDoctor($did, $date = null, $status = null)
+{
+    $conn = initDB();
+
+    $sql = "SELECT 
+                a.aptid, 
+                a.time, 
+                a.status, 
+                s.date, 
+                u.name AS patient_name
+            FROM appointment a
+            JOIN session s ON a.sid = s.sid
+            JOIN patient p ON a.pid = p.pid
+            JOIN user u ON p.uid = u.uid
+            WHERE s.did = '$did'";
+
+    if (!empty($date)) {
+        $date = mysqli_real_escape_string($conn, $date);
+        $sql .= " AND s.date = '$date'";
+    }
+
+    if (!empty($status)) {
+        $status = mysqli_real_escape_string($conn, $status);
+        $sql .= " AND a.status = '$status'";
+    }
+
+    $sql .= " ORDER BY s.date DESC, a.time ASC";
+
+    $result = mysqli_query($conn, $sql);
+    $data = [];
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+    }
+
+    return $data;
+}
+
+function updateAppointmentStatus($aptid, $status)
+{
+    $conn = initDB();
+    
+    $aptid = mysqli_real_escape_string($conn, $aptid);
+    $status = mysqli_real_escape_string($conn, $status);
+
+    $sql = "UPDATE appointment SET status='$status' WHERE aptid='$aptid'";
+
+    if (mysqli_query($conn, $sql)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+?>
