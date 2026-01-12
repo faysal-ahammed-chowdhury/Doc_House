326656da63587ad591174ccacc3b10c8c6686268
mehedi-hasan02 <mehedisani02@gmail.com>
2026-01-12 13:39:10 +0600

    make doctor model for add and get data

---
+++ model/Admin/DoctorModel.php
@@ -0,0 +1,61 @@
+<?php
+    require_once "C:\\xampp\htdocs\Doc_House\model\db.php";
+
+
+function addDoctorAdmin($data)
+{
+    $conn = initDB();
+
+    $uid  = (int)$data['uid'];
+    $fee  = (float)$data['fee'];
+    $bio  = mysqli_real_escape_string($conn, $data['bio']);
+    $spid = (int)$data['spid'];
+
+    $sql = "INSERT INTO doctor (uid, fee, bio, spid)
+            VALUES ($uid, $fee, '$bio', $spid)";
+
+
+    return mysqli_query($conn, $sql);
+}
+
+
+
+
+
+    function getAllDoctorsAdmin() {
+        $conn = initDB();
+
+        $sql = "SELECT 
+            d.uid,
+            u.name AS doctor_name,
+            u.email,
+            u.phone,
+            s.name AS specialization
+        FROM doctor d
+        JOIN user u ON d.uid = u.uid
+        JOIN specialization s ON d.spid = s.spid";
+
+        $result = mysqli_query($conn, $sql);
+
+        $doctors = [];
+        if(mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                $doctor = [];
+
+                $doctor['uid'] = $row['uid'];
+                $doctor['name'] = $row['doctor_name'];
+                $doctor['email'] = $row['email'];
+                $doctor['phone'] = $row['phone'];
+                $doctor['specialization'] = $row['specialization'];
+
+
+                array_push($doctors, $doctor);
+            }
+        }
+
+        return $doctors;
+    }
+
+
+
+?>
