5d2413c631591150d4c5194fd005a63778c7dc3f
Siyam Talukder <mdsiyamtalukder1@gmail.com>
2026-01-19 12:52:28 +0600

    update did

--- controller/Doctor/appointmentController.php
+++ controller/Doctor/appointmentController.php
@@ -1,13 +1,7 @@
 <?php
 require_once __DIR__ . '/../../model/Doctor/appointment_model.php';
 
-if (isset($_SESSION['user']['did'])) {
-    $doctor_id = $_SESSION['user']['did'];
-} elseif (isset($_SESSION['did'])) {
-    $doctor_id = $_SESSION['did'];
-} else {
-    $doctor_id = 11;
-}
+$doctor_id = $_SESSION['user']['did'] ?? 0;
 
 $filterDate = isset($_GET['date']) ? $_GET['date'] : '';
 $filterStatus = isset($_GET['status']) ? $_GET['status'] : '';
