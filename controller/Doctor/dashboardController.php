1ed636439994883e08bed761301e4d19da60bdf4
Siyam Talukder <mdsiyamtalukder1@gmail.com>
2026-01-19 12:47:40 +0600

    dash controller

--- controller/Doctor/dashboardController.php
+++ controller/Doctor/dashboardController.php
@@ -1,14 +1,8 @@
 <?php
-require_once __DIR__ . '/../../model/Doctor/dashboard_model.php';
+require_once "../../model/dashboard_model.php";
 
-if (isset($_SESSION['user']['did'])) {
-    $doctor_id = $_SESSION['user']['did'];
-} elseif (isset($_SESSION['did'])) {
-    $doctor_id = $_SESSION['did'];
-} else {
-    $doctor_id = 11;
-}
+$doctor_id = $_SESSION['user']['did'] ?? 0;
 
 $stats = getDashboardStats($doctor_id);
-$recentAppointments = getRecentAppointments($doctor_id, 3);
+$recentAppointments = getRecentAppointments($doctor_id);
 ?>
